381c532e8a33015583d46aa6985159b67905fa99
Gauta Raj <Gautam.Raj@quintiles.com>
2021-07-11 10:18:51 +0530

    immutability and cloning concept is implemented.

---
+++ src/main/java/lecture_02/homework/ImmutableClass.java
@@ -0,0 +1,16 @@
+package lecture_02.homework;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * ImmutableClass example
+ */
+public final class ImmutableClass {
+    private Integer id;
+    private String name;
+    private Date joiningDate;
+    private List<ImmutableAddress> addresses;
+
+
+}
